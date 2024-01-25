b997b34d5706879bc272f24a7c61e6b0a084387b
Mur1love <murilormleal@outlook.com>
2024-01-25 01:38:22 -0300

    finalized place detail

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,24 +1,33 @@
 import 'dart:io';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class PlaceLocation {
   final double latitude;
   final double longitude;
   final String? address;
 
-  const PlaceLocation({required this.latitude, required this.longitude, this.address,});
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    this.address,
+  });
+
+  LatLng toLatlng() {
+    // ignore: unnecessary_this
+    return LatLng(this.latitude, this.longitude);
+  }
 }
 
 class Place {
   final String id;
   final String title;
   final PlaceLocation? location;
-  final File image; 
+  final File image;
 
-  Place ({
-    required this.id,
-    required this.title,
-    required this.image,
-    required this.location
-  });
-
+  Place(
+      {required this.id,
+      required this.title,
+      required this.image,
+      required this.location});
 }
--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grand_places/models/place.dart';
+import 'package:grand_places/screens/map_screen.dart';
 
 class PlaceDetailScree extends StatelessWidget {
   const PlaceDetailScree({super.key});
@@ -10,6 +11,43 @@ class PlaceDetailScree extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(place.title),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 250,
+            width: double.infinity,
+            child: Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            place.location!.address as String,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextButton.icon(
+            icon: const Icon(Icons.map),
+            label: const Text('Ver no Mapa'),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => MapScreen(
+                    isReadOnly: true,
+                    initialLocation: place.location!,
+                  ),
+                ),
+              );
+            },
+          )
+        ],
       ),
     );
   }
